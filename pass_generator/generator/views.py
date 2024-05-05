e2b57056c98c958c9989c99d90291cf46b0c05ec
Alonesa <alenakrasnov@mail.ru>
2024-05-06 00:12:39 +0100

    generate random alpha password

--- pass_generator/generator/views.py
+++ pass_generator/generator/views.py
@@ -1,10 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import random
 
 # Create your views here.
-
+ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 def home(request):
     return render(request, 'generator/home.html')
 
 def password(request):
-    return render(request, 'generator/password.html')
+    thepassword = []
+    strength = 10
+    for x in range(strength):
+        thepassword.append(random.choice(ALPHABET))
+    for i in range(int(strength/2)):
+        el = random.choice(thepassword)
+        idx = thepassword.index(el)
+        thepassword[idx] = el.upper()
+
+    password = ''.join(random.sample(thepassword, len(thepassword)))
+    return render(request, 'generator/password.html', {'password':password})
