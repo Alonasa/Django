63bf5061e3de129b488989652d916959fda62801
Alonesa <alenakrasnov@mail.ru>
2024-05-09 21:36:39 +0100

    added styles

--- pass_generator/password_generator/settings.py
+++ pass_generator/password_generator/settings.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = 'password_generator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'generator' /'templates'],
+        'DIRS': [BASE_DIR / 'generator' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
