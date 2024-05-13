59cb3715dd0bf1ce1cf3cc435ecd0ef4b770e2d7
Alonesa <alenakrasnov@mail.ru>
2024-05-13 23:31:49 +0100

    attached css file and rendered own styles

--- pass_generator/password_generator/settings.py
+++ pass_generator/password_generator/settings.py
@@ -109,7 +109,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'generator' / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
