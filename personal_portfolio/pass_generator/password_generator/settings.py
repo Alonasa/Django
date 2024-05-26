10ba2c758d7bf1d3ca77bd2528ca85a915fcb1ff
Alonesa <alenakrasnov@mail.ru>
2024-05-26 19:25:20 +0100

    added header content block with basic text

--- personal_portfolio/pass_generator/password_generator/settings.py
+++ personal_portfolio/pass_generator/password_generator/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.static',
 ]
 
 MIDDLEWARE = [
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 
 STATIC_URL = '/static/'
