a9f81d93809b548159bfe70c98df9b56e8e52070
Alonesa <alenakrasnov@mail.ru>
2024-06-13 12:02:52 +0100

    created user database schema and check its work

--- personal_portfolio/travel_fellows/models.py
+++ personal_portfolio/travel_fellows/models.py
@@ -5,4 +5,13 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=120)
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=120, default='name')
+    surname = models.CharField(max_length=120, default='surename')
+    email = models.EmailField(max_length=100, unique=True, default='my@mail.com')
+    password = models.CharField(max_length=120, default='1111')
+
+
+class UserProfile(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
