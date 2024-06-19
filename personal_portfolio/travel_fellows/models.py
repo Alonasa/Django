45acd461fe6dc2bf235ac1976c4b76b7992d4b45
Alonesa <alenakrasnov@mail.ru>
2024-06-19 21:06:17 +0100

    added user plans schema

--- personal_portfolio/travel_fellows/models.py
+++ personal_portfolio/travel_fellows/models.py
@@ -27,3 +27,18 @@ class UserProfile(models.Model):
     transport = ArrayField(models.CharField(max_length=300), blank=False, default=list)
 
 
+class UserPlans(models.Model):
+    id = models.AutoField()
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    destinations = ArrayField(models.CharField(max_length=100), blank=False, default=list)
+    plans = ArrayField(models.CharField(max_length=1000), blank=False, default=list)
+    companions = models.IntegerField(default=1)
+    dates_start = models.DateField()
+    dates_end = models.DateField()
+    kids = models.BooleanField(default=False)
+
+
+
+
+
+
