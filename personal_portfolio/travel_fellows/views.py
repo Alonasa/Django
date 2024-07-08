3b18695f66a614ea2c373d6c56ce1bac6fe7f17d
Alonesa <alenakrasnov@mail.ru>
2024-07-08 23:59:46 +0100

    refactor registration and add user page

--- personal_portfolio/travel_fellows/views.py
+++ personal_portfolio/travel_fellows/views.py
@@ -34,15 +34,17 @@ class AuthorizeUser(View):
 
     def post(self, request):
         form = AuthorizeForm(request.POST)
+        backend = 'personal_portfolio.authentication.EmailAuthBackend'
         if form.is_valid():
             password = form.cleaned_data['password']
             email = form.cleaned_data['username']
             username = User.objects.filter(username=email).first()
             if username is not None:
-                user = authenticate(request, username=username, password=password, backend='personal_portfolio.authentication.EmailAuthBackend')
+                user = authenticate(request, username=username, password=password,
+                                    backend=backend)
 
                 if user is not None:
-                    login(request, user, backend='personal_portfolio.authentication.EmailAuthBackend')
+                    login(request, user, backend=backend)
                     return redirect('fellows')
                 return HttpResponse(f"USER NOT Logged, WRONG DATA AUTHUSER")
             else:
@@ -57,6 +59,9 @@ def logOut(request):
     return redirect('fellows')
 
 
+def userProfile(request, id):
+    return render(request, f"travel_fellows/destinations", {"id": id})
+
 class RegisterUser(View):
     def get(self, request):
         form = RegisterForm()
@@ -67,11 +72,13 @@ class RegisterUser(View):
     def post(self, request):
         form = RegisterForm(request.POST)
         if form.is_valid():
+            name = form.cleaned_data['name']
+            surname = form.cleaned_data['surname']
             password_confirm = form.cleaned_data['password']
             password = form.cleaned_data['password_confirm']
             email = form.cleaned_data['username']
             if password == password_confirm:
-                user = User.objects.create(name=form.cleaned_data['name'], surname=form.cleaned_data['surname'],
+                user = User.objects.create(name=name, surname=surname,
                                            username=email,
                                            password=password)
                 user.set_password(password)
